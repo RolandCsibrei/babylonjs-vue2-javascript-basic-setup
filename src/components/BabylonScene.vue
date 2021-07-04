52e22d7470b52b3172e566aadd21bbb6a53449b9
Roland Csibrei <roland.csibrei@nascor.tech>
2021-07-04 13:22:10 +0200

    Removal of HelloWorld.js

---
+++ src/components/BabylonScene.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <canvas ref="bjsCanvas" width="500" height="500" />
+  </div>
+</template>
+
+<script>
+import { createScene } from "../scenes/MyFirstScene";
+
+export default {
+  name: "BabylonScene",
+
+  mounted() {
+    const bjsCanvas = this.$refs.bjsCanvas;
+    if (bjsCanvas) {
+      createScene(bjsCanvas);
+    }
+  },
+};
+</script>
